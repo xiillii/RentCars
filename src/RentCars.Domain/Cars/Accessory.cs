43855de902aa768806e50f2a62f9a7fd1f4156dc
Jose Alonso <mail@josealonso.dev>
2024-02-23 23:02:42 -0600

    The initial Car entity

---
+++ src/RentCars.Domain/Cars/Accessory.cs
@@ -0,0 +1,10 @@
+﻿namespace RentCars.Domain.Cars;
+
+public enum Accessory
+{
+    WiFi = 1,
+    AirConditioning = 2,
+    AppleCar = 3,
+    AndroidCar = 4,
+    Maps = 5,
+}
---
+++ src/RentCars.Domain/Cars/Car.cs
@@ -0,0 +1,21 @@
+﻿namespace RentCars.Domain.Cars;
+
+public sealed class Car
+{
+    public Guid Id { get; private set; }
+    public string? Vin { get; private set; }
+    public string? Address { get; private set; }
+    public string? City { get; private set; }
+    public string? Country { get; private set; }
+
+    public decimal Price { get; private set; }
+    public string? Currency { get; private set; }
+
+    public decimal MaintenancePrice { get; private set; }
+    public string? MaintenanceCurrency { get; private set; }
+
+    public DateTime? LastDateRent { get; private set; }
+
+    public List<Accessory> Accessories { get; private set; } = new();
+
+}
